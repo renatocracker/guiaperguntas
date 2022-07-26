36e0efc7d88fdf39f4e822e993afcfb556ff6301
renato <renatocracker@hotmail.com>
2022-07-26 11:09:01 -0300

    primeiro comit

---
+++ database/pergunta.js
@@ -0,0 +1,17 @@
+const sequelize = require("sequelize");
+const connection = require("./database");
+
+const pergunta = connection.define('perguntas',{
+    titulo:{
+        type: sequelize.STRING,
+        allowNull: false
+    },
+    descricao: {
+        type: sequelize.TEXT,
+        allowNull: false
+    }    
+});
+
+pergunta.sync({force: false}).then(() => {});
+
+module.exports = pergunta;
